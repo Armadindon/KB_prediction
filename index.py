2922f7e9d1392dadfb3a6190f2eed473d242d6b9
Armadindon <baptisteperrin77@gmail.com>
2022-12-12 18:44:01 +0200

    Added submit endpoint

--- index.py
+++ index.py
@@ -3,18 +3,29 @@ import utils
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     index = ""
     with open("html/index.html") as f:
         index = f.read()
-    
+
     return index
+
 
 @app.route("/search")
 def search():
     searched = request.args.get("search")
-    return utils.search(searched)
+    return [name[0] for name in utils.search(searched)]
+
+
+@app.route("/submit", methods=['POST'])
+def submit():
+    print("Value submitted")
+    submitted = request.get_json(force=True)
+    print(submitted)
+    return jsonify({"status": "Ok"})
+
 
 if __name__ == "__main__":
     app.run()
